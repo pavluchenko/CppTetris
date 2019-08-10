66658afeb1a5c25f3e4698c27b310abff0545d24
Helga Pavluchenko <olga@pavluchenko.com>
2019-08-10 16:11:35 +0300

    change main

--- CppTetris/main.cpp
+++ CppTetris/main.cpp
@@ -1,15 +1,11 @@
-//
-//  main.cpp
-//  CppTetris
-//
-//  Created by Helga on 8/8/19.
-//  Copyright © 2019 Helga. All rights reserved.
-//
+#include "window.h"
+#include <QApplication>
 
-#include <iostream>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Window w;
+    w.show();
 
-using namespace std;
-
-int main(int argc, const char * argv[]) {
-    return 0;
+    return a.exec();
 }
