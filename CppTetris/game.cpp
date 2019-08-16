cd658a752dd54ac9753d5e0b4456022dce10a9cf
Helga Pavluchenko <olga@pavluchenko.com>
2019-08-16 17:28:02 +0300

    createNewPiece and drawPiece methods

--- CppTetris/game.cpp
+++ CppTetris/game.cpp
@@ -34,12 +34,41 @@ int Game::getRand (int pA, int pB) {
     return rand () % (pB - pA + 1) + pA;
 }
 
+void Game::createNewPiece() {
+    mPiece			= mNextPiece;
+    mRotation		= mNextRotation;
+    mPosX 			= (BOARD_WIDTH / 2) + mPieces->getXInitialPosition (mPiece, mRotation);
+    mPosY 			= mPieces->getYInitialPosition (mPiece, mRotation);
+
+    // Random next piece
+    mNextPiece 		= getRand (0, 6);
+    mNextRotation 	= getRand (0, 3);
+}
+
+void Game::drawPiece(int pX, int pY, int pPiece, int pRotation) {
+    color mColor;				// Color of the block
+
+    int mPixelsX = mBoard->getXPosInPixels (pX);
+    int mPixelsY = mBoard->getYPosInPixels (pY);
+
+    for (int i = 0; i < PIECE_BLOCKS; i++) {
+        for (int j = 0; j < PIECE_BLOCKS; j++) {
+            switch (mPieces->getBlockType (pPiece, pRotation, j, i)) {
+            case 1: mColor = GREEN; break;	// For each block of the piece except the pivot
+            case 2: mColor = BLUE; break;	// For the pivot
+            }
+        }
+    }
+
+}
 
 void Game::drawScene () {
 
 }
 
 void Game::drawBoard () {
-
+    int mX1 = BOARD_POSITION - (BLOCK_SIZE * (BOARD_WIDTH / 2)) - 1;
+        int mX2 = BOARD_POSITION + (BLOCK_SIZE * (BOARD_WIDTH / 2));
+        int mY = mScreenHeight - (BLOCK_SIZE * BOARD_HEIGHT);
 }
 
