253bdccb8497144a8da82a1bfa2ba7ad9b6cde1a
Helga Pavluchenko <olga@pavluchenko.com>
2019-08-16 17:20:49 +0300

    game cpp

--- CppTetris/game.cpp
+++ CppTetris/game.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "game.h"
+
+
+Game::Game(Board *pBoard, Pieces *pPieces, int pScreenHeight, IO* pIO) {
+    mScreenHeight = pScreenHeight;
+    mBoard = pBoard;
+    mPieces = pPieces;
+    mIO = pIO;
+    initGame();
+}
+
+void Game::initGame() {
+    // Init random numbers
+    srand ((unsigned int) time(NULL));
+
+    // First piece
+    mPiece			= getRand (0, 6);
+    mRotation		= getRand (0, 3);
+    mPosX 			= (BOARD_WIDTH / 2) + mPieces->getXInitialPosition (mPiece, mRotation);
+    mPosY 			= mPieces->getYInitialPosition (mPiece, mRotation);
+
+    //  Next piece
+    mNextPiece 		= getRand (0, 6);
+    mNextRotation 	= getRand (0, 3);
+    mNextPosX 		= BOARD_WIDTH + 5;
+    mNextPosY 		= 5;
+}
+
+int Game::getRand (int pA, int pB) {
+    return rand () % (pB - pA + 1) + pA;
+}
+
+
+void Game::drawScene () {
+
+}
+
+void Game::drawBoard () {
+
+}
+
--- CppTetris/game.h
+++ CppTetris/game.h
@@ -5,6 +5,7 @@
 #ifndef CPPTETRIS_GAME_H
 #define CPPTETRIS_GAME_H
 #include "board.h"
+#include "io.h"
 #include "pieces.h"
 #include <time.h>
 
@@ -12,13 +13,27 @@
 
 class Game {
 public:
-    Game (Board *pBoard, Pieces *pPieces, int pScreenHeight);
+    Game (Board *pBoard, Pieces *pPieces, int pScreenHeight, IO *mIO);
     
     void drawScene ();
     void createNewPiece ();
     
     int mPosX, mPosY;
     int mPiece, mRotation;
+
+private:
+    int mScreenHeight;				// Screen height in pixels
+    int mNextPosX, mNextPosY;		// Position of the next piece
+    int mNextPiece, mNextRotation;	// Kind and rotation of the next piece
+
+    Board *mBoard;
+    Pieces *mPieces;
+    IO *mIO;
+
+    int getRand (int pA, int pB);
+    void initGame();
+    void drawPiece (int pX, int pY, int pPiece, int pRotation);
+    void drawBoard ();
 };
 
 
